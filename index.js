a1644039e8097ea074041638606915bc9856d73b
Amy Jackson <aj81623@gmail.com>
2017-11-09 14:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(array){
   drivers.push('Ralph');
 }
+function destructivelyPrependDriver(array){
+  drivers.unshift('Bob');
+}
